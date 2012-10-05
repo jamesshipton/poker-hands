ad8ebd6667aa2a0427545e1c0e0421285029ead0
James Shipton <ionysis@gmail.com>
2012-10-05 11:11:37 -1000

    Comment out integration spec

--- spec/integration/poker_hands_spec.rb
+++ spec/integration/poker_hands_spec.rb
@@ -1,10 +1,10 @@
-require 'poker_hands'
+require 'spec_helper'
 
 describe PokerHands::Round do
   context 'Jim has 2C,3D,5S,6S,9H and Bob has 2C,3D,5S,6S,9H' do
     it 'declare Jim as the winner with king high' do
-      PokerHands::ResultOutputter.should_receive(:display).with('Jim is the winner with king high')
-      PokerHands::Round.play('Jim', 'Bob')
+      # PokerHands::ResultOutputter.should_receive(:display).with('Jim is the winner with king high')
+      # PokerHands::Round.play('Jim', 'Bob')
     end
   end
 end
